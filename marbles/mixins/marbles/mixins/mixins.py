4faceaf9d8d41a3798cb078a6b2c5836685f81b2
Jane Adams <jane@twosigma.com>
2018-05-19 13:28:29 -0400

    Updating copyright headers from TSI to TSOS

--- marbles/mixins/marbles/mixins/mixins.py
+++ marbles/mixins/marbles/mixins/mixins.py
@@ -1,12 +1,23 @@
 #
-#       Copyright (c) 2017 Two Sigma Investments, LP
-#       All Rights Reserved
+#  Copyright (c) 2018 Two Sigma Open Source, LLC
 #
-#       THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF
-#       Two Sigma Investments, LP.
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to
+#  deal in the Software without restriction, including without limitation the
+#  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+#  sell copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
 #
-#       The copyright notice above does not evidence any
-#       actual or intended publication of such source code.
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+#  IN THE SOFTWARE.
 #
 
 '''This module provides custom :mod:`unittest`-style assertions for
