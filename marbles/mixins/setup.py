113646c6a9b44d0bd0cb4de90b5b8612271d2375
Leif Walsh <leif.walsh@gmail.com>
2018-05-20 20:21:07 -0400

    Added classifiers, long_description, and project_urls to setup.pys (#56)
    Closes #46
    Closes #55

--- marbles/mixins/setup.py
+++ marbles/mixins/setup.py
@@ -24,9 +24,18 @@ import os.path
 from setuptools import setup
 
 
-with open(os.path.join(os.path.dirname(__file__),
-                       'marbles/mixins/VERSION')) as vfile:
+url = 'https://github.com/twosigma/marbles'
+setup_dir = os.path.dirname(__file__)
+root_dir = os.path.join(setup_dir, '..', '..')
+
+with open(os.path.join(setup_dir, 'marbles/mixins/VERSION')) as vfile:
     __version__ = vfile.read().strip()
+
+with open(os.path.join(root_dir, 'classifiers.txt'), 'r') as f:
+    classifiers = [line.strip() for line in f.readlines()]
+
+with open(os.path.join(root_dir, 'README.rst'), 'r') as f:
+    long_description = f.read()
 
 
 setup(
@@ -58,7 +67,15 @@ setup(
     license='MIT',
     description=('Semantically-rich assertions for use '
                  'in marbles and unittest test cases'),
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
     author='Jane Adams, Leif Walsh',
     author_email='jane@twosigma.com, leif@twosigma.com',
-    url='https://github.com/twosigma/marbles'
+    url=url,
+    project_urls={
+        'Documentation': 'https://marbles.readthedocs.io',
+        'Source': url,
+        'Tracker': '{url}/issues'.format(url=url)
+    },
+    classifiers=classifiers
 )
