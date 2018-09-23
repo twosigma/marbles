d6276bb77fcdc306080482ea48a32e03c3ab2bdd
Leif Walsh <leif@twosigma.com>
2018-09-22 21:10:40 -0400

    fix linenumbers, sigh

--- marbles/core/tests/test_marbles.py
+++ marbles/core/tests/test_marbles.py
@@ -527,7 +527,7 @@ class TestContextualAssertionError(MarblesTestCase):
         self.assertEqual(e.filename, os.path.abspath(__file__))
         # This isn't great because I have to change it every time I
         # add/remove imports but oh well
-        self.assertEqual(e.linenumber, 84)
+        self.assertEqual(e.linenumber, 83)
 
         with self.assertRaises(ContextualAssertionError) as ar:
             self.case.test_locals()
@@ -540,7 +540,7 @@ class TestContextualAssertionError(MarblesTestCase):
 
     def test_assert_stmt_indicates_line(self):
         '''Does e.assert_stmt indicate the line from the source code?'''
-        test_linenumber = 84
+        test_linenumber = 83
         test_filename = os.path.abspath(__file__)
         with self.assertRaises(ContextualAssertionError) as ar:
             self.case.test_failure()
@@ -559,7 +559,7 @@ class TestContextualAssertionError(MarblesTestCase):
 
     def test_assert_stmt_surrounding_lines(self):
         '''Does _find_assert_stmt read surrounding lines from the file?'''
-        test_linenumber = 84
+        test_linenumber = 83
         test_filename = os.path.abspath(__file__)
         with self.assertRaises(ContextualAssertionError) as ar:
             self.case.test_failure()
